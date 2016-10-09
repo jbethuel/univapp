1bb540506916c7295e7c727beaa474966bbe1564
Bethuel Dela Cruz <Bethuel Dela Cruz>
2016-10-09 13:15:53 +0800

    dashboards

--- client/dashboards/teacher/js/teacherclass.js
+++ client/dashboards/teacher/js/teacherclass.js
@@ -50,59 +50,11 @@ Template.teacherDashboardClass.events({
     template = "<div class='title_prompt'>Token copied to clipboard: "+passkey+"</div>";
     Meteor.Messages.dialog(title, template, button);
   },
-  'click [data-action=showActionSheet]': function(event, template) {
+  'click .class': function(event){
     var classIndexId = this._id;
-
-    IonActionSheet.show({
-      titleText: 'Actions',
-      buttons: [  {
-        text: 'View Class'
-      }],
-
-      destructiveText: 'Delete',
-      cancelText: 'Cancel',
-      cancel: function() {
-        console.log('Cancelled!');
-      },
-      buttonClicked: function(index) {
-
-        if (index == 0) {
-          console.log(classIndexId);
-          Router.go('teacherDashboardViewClass',{
-            class_id: classIndexId
-          });
-          // Router.go('teacherDashboardSeatplan', {
-          //   class_id: classIndexId
-          // });
-        }
-        return true;
-      },
-      destructiveButtonClicked: function() {
-        console.log('Destructive Action!');
-
-        IonPopup.prompt({
-          title: 'Delete Class',
-          template: "<div class='title_prompt'>You're about to delete this class, please type CONFIRM</div>",
-          okText: 'Submit',
-          inputType: 'Text',
-            onOk: function(error, result){
-              token = result;
-              if(token === "CONFIRM"){
-                Meteor.call("deleteClass", classIndexId);
-                IonLoading.show({
-                  customTemplate: '<p>The class was successfully deleted.</p>',
-                  duration: 3000
-                });
-              }else{
-                IonLoading.show({
-                  customTemplate: '<h4>ERROR</h4><p>The class was not successfully deleted.</p>',
-                  duration: 3000
-                });
-              }
-            }
-        });
-        return true;
-      }
+    console.log(classIndexId);
+    Router.go('teacherDashboardViewClass',{
+      class_id: classIndexId
     });
   }
 });
--- client/dashboards/teacher/js/teacherviewclass.js
+++ client/dashboards/teacher/js/teacherviewclass.js
@@ -2,10 +2,6 @@ Template.teacherDashboardViewClass.onCreated(function(){
   this.subscribe("classindex");
 });
 
-Template.teacherDashboardViewClass.onDestroyed(function(){
-  delete Session.keys["classId"];
-});
-
 Template.teacherDashboardViewClass.helpers({
   classid: function(){
     return this.class_id;
@@ -14,3 +10,55 @@ Template.teacherDashboardViewClass.helpers({
     return classindex.find({_id:classid}).fetch();
   }
 });
+
+Template.teacherDashboardViewClass.events({
+  "click #teacherAnnouncement": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardAnnouncement",{
+      class_id: id
+    });
+  },
+  "click #teacherActivity": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardActivity",{
+      class_id: id
+    });
+  },
+  "click #teacherSeatplan": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardSeatplan",{
+      class_id: id
+    });
+  },
+  "click #teacherAttendance": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardAttendance",{
+      class_id: id
+    });
+  },
+  "click #teacherGraderecords": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardRecords",{
+      class_id: id
+    });
+  },
+  "click #teacherMessage": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardMessageboard",{
+      class_id: id
+    });
+  },
+  "click #teacherSettings": function(event){
+    event.preventDefault();
+    id = Router.current().params.class_id;
+    Router.go("teacherDashboardGradeSettings",{
+      class_id: id
+    });
+  }
+})
